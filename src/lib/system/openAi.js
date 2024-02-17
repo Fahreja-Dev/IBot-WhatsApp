0dbf1d7eb6660e451052663cbd87fb5ffccea774
Fahreja.Dev <fahreja.dev@gmail.com>
2024-02-17 21:04:04 +0700

    Memperbarui untuk fitur multi API OpenAI

--- src/lib/system/openAi.js
+++ src/lib/system/openAi.js
@@ -1,16 +1,31 @@
 import OpenAI from "openai";
 import { manageBot } from "../../config.js";
+import { multiOpenAi, switchApiOpenAi } from "./multiOpenAi.js";
+import { multiApiOpenAi } from "../../multiApi.js";
 
-const openai = new OpenAI({
-  organization: manageBot.organizationOpenAi,
-  apiKey: manageBot.apiKeyOpenAi,
-});
-
-export async function openAI(message) {
+export async function openAI(message, number = 0) {
   try {
+    const api = switchApiOpenAi(
+      manageBot,
+      multiOpenAi(multiApiOpenAi, number, manageBot.multiApiKey),
+      manageBot.multiApiKey
+    );
+
+    const openai = new OpenAI({
+      organization: api.organizationOpenAi,
+      apiKey: api.apiKeyOpenAi,
+    });
+
+    let resultUser = "";
+    let responseUser = await Promise.all(message);
+
+    for (const x of responseUser) {
+      resultUser += await x;
+    }
+
     const stream = await openai.chat.completions.create({
       model: "gpt-3.5-turbo",
-      messages: [{ role: "user", content: message }],
+      messages: [{ role: "user", content: resultUser }],
       stream: true,
     });
 
@@ -20,7 +35,14 @@ export async function openAI(message) {
       output += (await chunk.choices[0]?.delta?.content) || "";
     }
 
-    return output;
+    let outputResult = "";
+    let outputResponse = await Promise.all(output);
+
+    for await (const y of outputResponse) {
+      outputResult += await y;
+    }
+
+    return outputResult;
   } catch (err) {
     return "Telah mencapai batas limit, silahkan coba lagi nanti!";
   }
