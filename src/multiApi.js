91f5ace247bbe3ef30631944fa221a8cab43629c
Fahreja.Dev <fahreja.dev@gmail.com>
2024-02-17 20:51:07 +0700

    Menambahkan file [multiApi.js] untuk fitur multi API

---
+++ src/multiApi.js
@@ -0,0 +1,9 @@
+export const multiApiOpenAi = {
+  /* Pengaturan Multi API OpenAI */
+  /* Hapus komentar atau tanda garis miring untuk menambahkan multi API kamu. dan kamu bisa membuatnya lebih banyak lagi dengan menambahkan objek [api] secara berurutan */
+  api1: ["ApiKey kamu", "Organization kamu"],
+  api2: ["ApiKey kamu", "Organization kamu"],
+  api3: ["ApiKey kamu", "Organization kamu"],
+  //   api4: ["ApiKey kamu", "Organization kamu"],
+  //   api5: ["ApiKey kamu", "Organization kamu"],
+};
