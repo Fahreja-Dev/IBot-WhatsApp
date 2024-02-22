fb7029fccfbd3a7c4553639199d710f95d8a5ea1
Fahreja.Dev <fahreja.dev@gmail.com>
2024-02-22 17:32:08 +0700

    Menambahkan pengaturan objek multiApiKeyGemini & apiKeyGeminiAi

--- src/config.js
+++ src/config.js
@@ -3,4 +3,8 @@ export const manageBot = {
   multiApiKey: true, // Untuk mengaktifkan fitur multi API OpenAI, kamu bisa atur API nya di file [multiApi.js]
   organizationOpenAi: "Organization kamu",
   apiKeyOpenAi: "ApiKey kamu",
+  /* <====================================================================> */
+  /* Pengaturan Gemini AI */
+  multiApiKeyGemini: true,
+  apiKeyGeminiAi: "ApiKey kamu",
 };
