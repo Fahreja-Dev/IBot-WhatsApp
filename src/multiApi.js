c0f64b5bddc4181d09f633f768f20ce5407e5904
Fahreja.Dev <fahreja.dev@gmail.com>
2024-02-22 17:21:02 +0700

    Menambahkan objek multi API, untuk fitur Gemini AI

--- src/multiApi.js
+++ src/multiApi.js
@@ -7,3 +7,13 @@ export const multiApiOpenAi = {
   //   api4: ["ApiKey kamu", "Organization kamu"],
   //   api5: ["ApiKey kamu", "Organization kamu"],
 };
+
+export const multiApiGeminiAi = {
+  /* Pengaturan Multi API GeminiAI */
+  /* Hapus komentar atau tanda garis miring untuk menambahkan multi API kamu. dan kamu bisa membuatnya lebih banyak lagi dengan menambahkan objek [api] secara berurutan */
+  api1: ["ApiKey kamu"],
+  api2: ["ApiKey kamu"],
+  api3: ["ApiKey kamu"],
+  // api4: ["ApiKey kamu"],
+  // api5: ["ApiKey kamu"],
+};
