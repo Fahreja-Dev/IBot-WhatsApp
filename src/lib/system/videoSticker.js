fd93cafb74d01117a1cc5037dba68fd3b04dda27
Fahreja.Dev <fahreja.dev@gmail.com>
2024-03-10 05:35:33 +0700

    Menambahkan pesan kesalahan pada fitur Video Convert Sticker

--- src/lib/system/videoSticker.js
+++ src/lib/system/videoSticker.js
@@ -11,51 +11,57 @@ const Stream = stream;
 const Crypto = crypto;
 
 export async function videoSticker(media, message, option) {
-  if (!media.mimetype.includes("video"))
-    throw new Error("media is not a video");
-  const videoType = media.mimetype.split("/")[1];
-  const tempFile = path.join(
-    os.tmpdir(),
-    `${Crypto.randomBytes(6).readUIntLE(0, 6).toString(36)}.webp`
-  );
-  const stream = new Stream.Readable();
-  const buffer = Buffer.from(
-    media.data.replace(`data:${media.mimetype};base64,`, ""),
-    "base64"
-  );
-  stream.push(buffer);
-  stream.push(null);
-  const x = await new Promise((resolve, reject) => {
-    ffmpeg(stream)
-      .inputFormat(videoType)
-      .on("error", reject)
-      .on("end", () => resolve(true))
-      .addOutputOptions([
-        "-vcodec",
-        "libwebp",
-        "-vf",
-        "scale='iw*min(300/iw,300/ih)':'ih*min(300/iw,300/ih)',format=rgba,pad=300:300:'(300-iw)/2':'(300-ih)/2':'#00000000',setsar=1,fps=10",
-        "-loop",
-        "0",
-        "-ss",
-        "00:00:00.0",
-        "-t",
-        "00:00:09.0",
-        "-preset",
-        "default",
-        "-an",
-        "-vsync",
-        "0",
-        "-s",
-        "512:512",
-      ])
-      .toFormat("webp")
-      .save(tempFile);
-  });
-  const data = fs.readFileSync(tempFile, "base64");
-  fs.unlinkSync(tempFile);
+  try {
+    if (!media.mimetype.includes("video"))
+      throw new Error("media is not a video");
+    const videoType = media.mimetype.split("/")[1];
+    const tempFile = path.join(
+      os.tmpdir(),
+      `${Crypto.randomBytes(6).readUIntLE(0, 6).toString(36)}.webp`
+    );
+    const stream = new Stream.Readable();
+    const buffer = Buffer.from(
+      media.data.replace(`data:${media.mimetype};base64,`, ""),
+      "base64"
+    );
+    stream.push(buffer);
+    stream.push(null);
+    const x = await new Promise((resolve, reject) => {
+      ffmpeg(stream)
+        .inputFormat(videoType)
+        .on("error", reject)
+        .on("end", () => resolve(true))
+        .addOutputOptions([
+          "-vcodec",
+          "libwebp",
+          "-vf",
+          "scale='iw*min(300/iw,300/ih)':'ih*min(300/iw,300/ih)',format=rgba,pad=300:300:'(300-iw)/2':'(300-ih)/2':'#00000000',setsar=1,fps=10",
+          "-loop",
+          "0",
+          "-ss",
+          "00:00:00.0",
+          "-t",
+          "00:00:09.0",
+          "-preset",
+          "default",
+          "-an",
+          "-vsync",
+          "0",
+          "-s",
+          "512:512",
+        ])
+        .toFormat("webp")
+        .save(tempFile);
+    });
+    const data = fs.readFileSync(tempFile, "base64");
+    fs.unlinkSync(tempFile);
 
-  const mediaSticker = new MessageMedia("image/webp", data);
+    const mediaSticker = new MessageMedia("image/webp", data);
 
-  await message.reply(mediaSticker, message.from, option);
+    await message.reply(mediaSticker, message.from, option);
+  } catch (err) {
+    await message.reply(
+      "Terjadi kesalahan saat proses convert, silahkan coba lagi!"
+    );
+  }
 }
