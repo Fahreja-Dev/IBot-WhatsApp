57e640bac1f54202d72f6d9ba9cda80eaf1670ef
Fahreja.Dev <fahreja.dev@gmail.com>
2024-03-17 11:57:58 +0700

    Menambahkan pengaturan objek accessTokenLyricMusic & Memperbarui Komentar

--- src/config.js
+++ src/config.js
@@ -1,13 +1,20 @@
 export const manageBot = {
   /* Pengaturan Open AI */
+  /* Website untuk mendapatkan API & Organization : https://platform.openai.com/api-keys */
   multiApiKey: true, // Untuk mengaktifkan fitur multi API OpenAI, kamu bisa atur API nya di file [multiApi.js]
   organizationOpenAi: "Organization kamu",
   apiKeyOpenAi: "ApiKey kamu",
   /* <====================================================================> */
   /* Pengaturan Gemini AI */
-  multiApiKeyGemini: true,
+  /* Website untuk mendapatkan API : https://aistudio.google.com/app/apikey */
+  multiApiKeyGemini: true, // Untuk mengaktifkan fitur multi API GeminiAI, kamu bisa atur API nya di file [multiApi.js]
   apiKeyGeminiAi: "ApiKey kamu",
   /* <====================================================================> */
   /* Pengaturan search Image Unsplash*/
-  accessKeyUnsplash: "Access Key kamu"
+  /* Website untuk mendapatkan Access Key : https://unsplash.com/oauth/applications */
+  accessKeyUnsplash: "Access Key kamu",
+  /* <====================================================================> */
+  /* Pengaturan Lyric Music*/
+  /* Website untuk mendapatkan Access Token : https://genius.com/api-clients */
+  accessTokenLyricMusic: "Access Token kamu"
 };
