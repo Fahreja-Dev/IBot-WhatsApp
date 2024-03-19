16b2d72b241c4a844471122a15c2190f46f1571e
Fahreja.Dev <fahreja.dev@gmail.com>
2024-03-19 14:17:42 +0700

    Menambahkan file [proxy.js], untuk pengaturan fitur Socks Proxy

---
+++ src/proxy.js
@@ -0,0 +1,8 @@
+export const proxy = {
+    active: false, // Untuk mengaktifkan socks proxy (true = aktif | false = tidak aktif)
+    host: "", // Ganti dengan hostname atau IP socks server kamu
+    port: 1080, // Ganti dengan port socks proxy server kamu
+    username: "", // Ganti dengan username atau userid socks proxy server kamu
+    password: "", // Ganti dengan password socks proxy server kamu, dan bisa kosongkan jika menggunakan socks4
+    socks: 5 // Untuk menjalankan socks4 atau socks5 (4 = socks4 | 5 = socks5)
+}
