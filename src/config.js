739665763aaec0981981a600883b659243e96f4b
Fahreja.Dev <fahreja.dev@gmail.com>
2024-03-19 14:20:55 +0700

    Memperbarui Deskripsi Pengaturan API

--- src/config.js
+++ src/config.js
@@ -1,20 +1,22 @@
 export const manageBot = {
-  /* Pengaturan Open AI */
-  /* Website untuk mendapatkan API & Organization : https://platform.openai.com/api-keys */
+  /* Pengaturan Open AI
+  Website untuk mendapatkan API & Organization : https://platform.openai.com/api-keys */
   multiApiKey: true, // Untuk mengaktifkan fitur multi API OpenAI, kamu bisa atur API nya di file [multiApi.js]
   organizationOpenAi: "Organization kamu",
   apiKeyOpenAi: "ApiKey kamu",
   /* <====================================================================> */
-  /* Pengaturan Gemini AI */
-  /* Website untuk mendapatkan API : https://aistudio.google.com/app/apikey */
+  /* ~Pengaturan Gemini AI~
+  Website untuk mendapatkan API : https://aistudio.google.com/app/apikey */
   multiApiKeyGemini: true, // Untuk mengaktifkan fitur multi API GeminiAI, kamu bisa atur API nya di file [multiApi.js]
   apiKeyGeminiAi: "ApiKey kamu",
   /* <====================================================================> */
-  /* Pengaturan search Image Unsplash*/
-  /* Website untuk mendapatkan Access Key : https://unsplash.com/oauth/applications */
+  /* ~Pengaturan search Image Unsplash~
+  Website untuk mendapatkan Access Key : https://unsplash.com/oauth/applications */
   accessKeyUnsplash: "Access Key kamu",
   /* <====================================================================> */
-  /* Pengaturan Lyric Music*/
-  /* Website untuk mendapatkan Access Token : https://genius.com/api-clients */
+  /* ~Pengaturan Lyric Music~
+   Website untuk mendapatkan Access Token : https://genius.com/api-clients
+   Gunakan socks proxy untuk mengaktifkan fitur Lyric Music jika terjadi error, atur proxy kamu di file [proxy.js]
+  */
   accessTokenLyricMusic: "Access Token kamu"
 };
