ec41c9ea85941255e889aa160b77c267ecaabde3
Fahreja.Dev <fahreja.dev@gmail.com>
2024-08-01 19:34:37 +0700

    Memperbarui versi whatsapp-web.js

--- src/run.js
+++ src/run.js
@@ -21,10 +21,6 @@ const client = new Client({
     executablePath: '/usr/bin/google-chrome-stable',
     args: ["--no-sandbox", "--disable-setuid-sandbox"],
   },
-  webVersionCache: {
-    type: 'remote',
-    remotePath: 'https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2412.54.html',
-  },
 });
 
 client.on("qr", (qr) => {
